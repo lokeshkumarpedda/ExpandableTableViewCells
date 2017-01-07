7564150c983900107a3055b18a9355ce47f712a0
Next <next@swathigupta.local>
2017-01-07 18:36:59 +0530

    1 st logic

--- ExpandableTableViewCells/SecondVC.swift
+++ ExpandableTableViewCells/SecondVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondVC.swift
 //  ExpandableTableViewCells
 //
 //  Created by Next on 06/01/17.
@@ -8,24 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
-    
+class SecondVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
-    
-    var parentArray = ["First", "Second", "Third"]
+    var parentArray = ["First", "Second", "Third","Fourth","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]
     let childArray = ["1","2"]
-    var headerCells = [true,true,true]
-    var isExpandedCells = [false, false, false]
-    var headerArray = ["First", "Second", "Third"] // For original parent Array reference
+    var headerCells = [true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true]
+    var isExpandedCells = [false, false, false, false,false, false, false, false,false, false, false, false,false, false, false, false,false, false, false, false]
+    var headerArray = ["First", "Second", "Third","Fourth","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"] // For original parent Array reference
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     func collapse() {
         for i in 0 ..< isExpandedCells.count{
             
@@ -47,7 +46,6 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         for i in 1 ... childArray.count {
             parentArray.remove(at: indexPath.row+1)
             headerCells.remove(at: indexPath.row+1)
-            
             tableView.deleteRows(at: [NSIndexPath.init(row: indexPath.row+i, section: 0) as IndexPath], with: .fade)
         }
         tableView.endUpdates()
@@ -58,7 +56,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         }
     }
     
-    //MARK:- TableView methods
+    //
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return parentArray.count
@@ -72,6 +70,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
         }
         else{
             cell?.textLabel?.text = parentArray[indexPath.row]
+            cell?.textLabel?.textColor = UIColor.blue
             cell?.isUserInteractionEnabled = false
         }
         return cell!
@@ -89,6 +88,7 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
             tableView.endUpdates()
             headerCells[indexPath.row] = false
             let value = parentArray[indexPath.row]
+            
             collapse() // collapsing already expanded cells
             
             if let index = headerArray.index(of: value){
@@ -100,5 +100,6 @@ class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSou
             deletetingRows(indexPath : indexPath as NSIndexPath)
         }
     }
-}
 
+
+}
